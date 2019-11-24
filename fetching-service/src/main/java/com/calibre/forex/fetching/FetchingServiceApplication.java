09be87e2c72c63a2fadcc0fd1c44716996714d25
Ryan <ryanwangpost@gmail.com>
2019-11-24 13:31:30 +1100

    first commit for project

--- fetching-service/src/main/java/com/calibre/forex/fetching/FetchingServiceApplication.java
+++ fetching-service/src/main/java/com/calibre/forex/fetching/FetchingServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.util.ClassUtils;
 
 @SpringBootApplication
 @EnableCaching
